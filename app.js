3d851f4f28763a74280f4e638956e2d2491ed9c2
LeBaoChi <lebaochi1809@gmail.com>
2018-01-09 19:27:41 +0700

    8000

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
-var port= process.env.PORT || 3000;
+var port= process.env.PORT || 8000;
 
 var app = express();
 
